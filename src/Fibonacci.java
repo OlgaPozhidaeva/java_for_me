84b20bce5f58e68fa66a525afc6617601bba2ff2
OlgaPozhidaeva <olga.igorevna.pozhidaeva@gmail.com>
2019-12-08 22:20:12 +0400

    HW1

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,11 +1,11 @@
 public class Fibonacci {
 
     public static void main(String[] args) {
-     int one = 0;
-     int next = 1;
-     for (int i = 0; i < 10; i++){
-         next = one + next;
-         one = next - one;
+        int one = 0;
+        int next = 1;
+        for (int i = 0; i < 10; i++) {
+            next = one + next;
+            one = next - one;
             System.out.println(next);
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int factorial = 1;
+        for (int i = 1; i <= 6; i++) {
+
+            factorial = i * factorial;
+        }
+        System.out.println("factorial " + factorial);
     }
 }
